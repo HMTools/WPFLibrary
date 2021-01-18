f881a0c98d2e1da6e740db8bf187358ba2a3ae21
HMTools <m4t4ngm@gmail.com>
2021-01-18 13:54:19 +0200

    Add CharToStringConverter

---
+++ WPFLibrary/ValueConverters/CharToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace WPFLibrary.ValueConverters
+{
+    public class CharToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return value.ToString();
+            }
+            catch
+            {
+                return Binding.DoNothing;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var str = value as string;
+            if (str != null && str.Length > 0)
+            {
+                return (value as string)[0];
+            }
+            return Binding.DoNothing;
+        }
+    }
+}
