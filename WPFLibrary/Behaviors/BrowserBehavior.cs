7fae64253db597e9ec92764f35cc512424e79450
HMTools <m4t4ngm@gmail.com>
2021-03-20 00:44:51 +0200

    Added Browser Behavior with HTML property

---
+++ WPFLibrary/Behaviors/BrowserBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFLibrary.Behaviors
+{
+    /*
+     * Credits On HtmlProperty Goes To Fredrick Hedblad https://stackoverflow.com/a/4204350
+     * 
+     */
+    public class BrowserBehavior
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+            "Html",
+            typeof(string),
+            typeof(BrowserBehavior),
+            new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser d)
+        {
+            return (string)d.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser d, string value)
+        {
+            d.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            if (dependencyObject is WebBrowser webBrowser)
+                webBrowser.NavigateToString(e.NewValue as string ?? "&nbsp;");
+        }
+    }
+}
