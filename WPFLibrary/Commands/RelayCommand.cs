b13c1b5785b53eb6ea37ebd53345e44682a3f91d
HMTools <m4t4ngm@gmail.com>
2022-05-28 10:52:03 +0300

    Created generic RelayCommand implementation

--- WPFLibrary/Commands/RelayCommand.cs
+++ WPFLibrary/Commands/RelayCommand.cs
@@ -32,4 +32,32 @@ namespace WPFLibrary.Commands
             this.execute(parameter);
         }
     }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> execute;
+        private Func<T, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute((T)parameter);
+        }
+    }
 }
